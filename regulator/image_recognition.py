98dfaa8ebe78ad3b7aa3cf7a4328a9f83d9258d1
Wiktor Guź <wiktorguz@gmail.com>
2019-09-14 20:08:08 +0200

    Circle and colors

--- regulator/image_recognition.py
+++ regulator/image_recognition.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2# as cv
 #import matplotlib.pyplot as plt
 img1 = cv2.imread('0.png', cv2.IMREAD_COLOR)
-hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
 gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
 
@@ -34,7 +33,7 @@ def findCircle(img):
 
         biggest_circle = circles[0]
 
-        print(circles)
+        #print(circles)
 
         # # loop over the (x, y) coordinates and radius of the circles
         # for (x, y, r) in circles:
@@ -50,22 +49,22 @@ def findCircle(img):
 
 def findColor(img, lower, upper):
 
-    green = cv2.inRange(hsv, lower, upper)
+    in_range = cv2.inRange(img, lower, upper)
 
     kernal = np.ones((5 ,5), "uint8")
-    blue=cv2.dilate(green, kernal)
-    res=cv2.bitwise_and(img1, img1, mask = blue)
+    dilated=cv2.dilate(in_range, kernal)
+    res=cv2.bitwise_and(img1, img1, mask = dilated)
 
     #cv2.imshow("", res)
 
-    (_, contours, hierarchy) = cv2.findContours(green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    (_, contours, hierarchy) = cv2.findContours(in_range, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     x = 0
     y = 0
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
         if (area > 100):
             x, y, w, h = cv2.boundingRect(contour)
-            img = cv2.rectangle(img1, (x, y), (x + w, y + h), (255, 0, 0), 3)
+            #img = cv2.rectangle(img1, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
     return x, y
 
@@ -85,18 +84,32 @@ white_upper = np.array([255,sens,255], np.uint8)
 resized, circle = findCircle(gray)
 x, y, r = (int(x) for x in circle)
 
+height,width,depth = img1.shape
+circle_img = np.zeros((height,width), np.uint8)
+cv2.circle(circle_img,(x,y),r,1,thickness=-1)
 
-cv2.circle(img1, (x, y), r, (0, 255, 0), 4)
-cv2.rectangle(img1, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+masked_data = cv2.bitwise_and(img1, img1, mask=circle_img)
+#masked_data = img1
 
-findColor(hsv, green_lower, green_upper)
 
-findColor(hsv, magenta_lower, magenta_upper)
 
-findColor(hsv, white_lower, white_upper)
+#cv2.circle(img1, (x, y), r, (0, 255, 0), 4)
+#cv2.rectangle(img1, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+masked_data_hsv = cv2.cvtColor(masked_data, cv2.COLOR_BGR2HSV)
+
+g_rect = findColor(masked_data_hsv, green_lower, green_upper)
+
+m_rect = findColor(masked_data_hsv, magenta_lower, magenta_upper)
+
+w_rect = findColor(masked_data_hsv, white_lower, white_upper)
+
+cv2.rectangle(masked_data, (g_rect[0], g_rect[1]), (g_rect[0]+10, g_rect[1]+10), (255, 0, 0), 3)
+cv2.rectangle(masked_data, (m_rect[0], m_rect[1]), (m_rect[0]+10, m_rect[1]+10), (255, 0, 0), 3)
+cv2.rectangle(masked_data, (w_rect[0], w_rect[1]), (w_rect[0]+10, w_rect[1]+10), (255, 0, 0), 3)
 
 cv2.namedWindow("Color Tracking1")
-cv2.imshow("Color Tracking1", img1)
+cv2.imshow("Color Tracking1", masked_data)
 
 #cv2.namedWindow("Color Tracking")
 #cv2.imshow("Color Tracking", res)
