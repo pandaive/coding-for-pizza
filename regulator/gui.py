97901768a5bf2b2f69978d7f7bc24fb1f6329bd7
Wiktor Guź <wiktorguz@gmail.com>
2019-09-15 11:16:14 +0200

    WIP

--- regulator/gui.py
+++ regulator/gui.py
@@ -7,18 +7,18 @@ class MyFrame(wx.Frame):
         panel = wx.Panel(self)
 
         self.dig_ctrl_trun = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='TURN', pos=(5, 30))
+        my_btn = wx.Button(panel, label='TURN', pos=(5, 40))
 
 
-        my_btn = wx.Button(panel, label='STOP', pos=(5, 55))
+        my_btn = wx.Button(panel, label='STOP', pos=(5, 65))
 
-        self.dig_ctrl_speed = wx.TextCtrl(panel, pos=(5, 80))
-        my_btn = wx.Button(panel, label='SPEED', pos=(5, 105))
+        self.dig_ctrl_speed = wx.TextCtrl(panel, pos=(5, 100))
+        my_btn = wx.Button(panel, label='SPEED', pos=(5, 140))
 
-        self.dig_ctrl_pause = wx.TextCtrl(panel, pos=(5, 130))
-        my_btn = wx.Button(panel, label='PAUSE', pos=(5, 155))
+        self.dig_ctrl_pause = wx.TextCtrl(panel, pos=(5, 180))
+        my_btn = wx.Button(panel, label='PAUSE', pos=(5, 220))
 
-        my_btn = wx.Button(panel, label='BASE', pos=(5, 190))
+        my_btn = wx.Button(panel, label='BASE', pos=(5, 255))
 
         self.Show()
         #if int(self.dig_ctrl.GetValue())
